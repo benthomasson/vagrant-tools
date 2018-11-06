a2ff86d3304bd2f4c6cb9303219b000d960d1687
Ben Thomasson <bthomass@redhat.com>
2018-11-06 07:56:02 -0500

    Adds vagrant_up and vagrant_ssh_config

---
+++ action_plugins/vagrant_up.py
@@ -0,0 +1,15 @@
+from ansible.plugins.action import ActionBase
+
+import subprocess
+
+
+class ActionModule(ActionBase):
+
+    BYPASS_HOST_LOOP = True
+
+    def run(self, tmp=None, task_vars=None):
+        if task_vars is None:
+            task_vars = dict()
+        result = super(ActionModule, self).run(tmp, task_vars)
+        subprocess.check_call('vagrant up', shell=True)
+        return result
